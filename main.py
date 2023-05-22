8c4af90e054c94fa778c017f62c2d88ed0eaee6e
Bohdan Markiv <b.markiv@ruitenburg.nu>
2023-05-22 17:41:59 +0200

    -refactored file location

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from functools import reduce
 from DataCreation import DataCreation
-from descriptive import descriptive_stats
+from helper import descriptive_stats, generate_growth_data
 base_fp = "./data/"
 exm = DataCreation(base_fp)
 exm.compose_df()
@@ -9,14 +9,4 @@ tb= exm.final_db
 
 #descriptive_stats(data=exm, folder_desc="descriptive_ind", main_name="All_the_means")
         
-countries = list(tb["Country"].unique())
-columns = list(tb.columns)
-columns.remove("Year").remove("GDP")
-
-for country in countries:
-    df = tb[(tb["Country"] == country) & (tb["Year"] >=1999)& (tb["Year"] <=2020)]
-    for column in columns:
-        df[column] = df[column] * 0.01 * df["GDP"]
-        
-    
-    df.to_excel("./output/"+country+"(fin).xlsx")
+generate_growth_data(tb)
