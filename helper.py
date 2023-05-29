896c6912fb468c6e4b3b330e95647e23c7a34c4b
Bohdan Markiv <b.markiv@ruitenburg.nu>
2023-05-29 16:16:42 +0200

    updates

--- helper.py
+++ helper.py
@@ -2,41 +2,64 @@ import pandas as pd
 
 def descriptive_stats(data, folder_desc, main_name):
 #Code for descriptive stats
-    tb= data.final_db
-    cols = list(tb.columns)
+    cols = list(data.columns)
     cols.remove("Country")
     cols.remove("Year")
 
-    tb = tb[tb["Year"] >=2000]
-    countries=exm.my_countries
+    countries = (data["Country"].unique())
     fin = pd.DataFrame({"index":['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']})
     overall = pd.DataFrame(index=countries, columns=cols)
     for state in countries:
         for col in cols:
-            fin = fin.merge(tb[tb["Country"] ==state][col].describe().reset_index(), on=["index"])
-            overall[col].loc[state] = tb[tb["Country"] ==state][col].mean()
+            fin = fin.merge(data[data["Country"] ==state][col].describe().reset_index(), on=["index"])
+            overall[col].loc[state] = data[data["Country"] ==state][col].mean()
             print(fin)
-        fin["Country"] = state 
+        fin["Country"] = state
         fin.to_excel("./" + folder_desc +"/"+state+"_descriptive_ind.xlsx")
         fin = pd.DataFrame({"index":['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']})
     overall.to_excel(main_name + ".xlsx")
     
-def generate_growth_data(data):
+def generate_growth_data(data, year_start, year_finish):
     countries = list(data["Country"].unique())
-    countries.remove("Turkey")
-    countries.remove("Norway")
+    #countries.remove("Turkey")
+    #countries.remove("Norway")
     columns = list(data.columns)
     columns.remove("Year")
     columns.remove("GDP")
     columns.remove("Country")
 
+    result = pd.DataFrame()
 
     for country in countries:
-        df = data[(data["Country"] == country) & (data["Year"] >=1999)& (data["Year"] <=2020)]
+        df = data[(data["Country"] == country) & (data["Year"] >=year_start)& (data["Year"] <=year_finish)]
         for column in columns:
             df[column] = df[column] * 0.01 * df["GDP"]
             df[column] = (df[column] - df[column].shift(1))/df[column].shift(1)
             
         df["GDP"] = (df["GDP"] - df["GDP"].shift(1))/df["GDP"].shift(1)
+        result = pd.concat([result, df])
+    return result
         
-        df.to_excel("./output/"+country+"(growth).xlsx")
+
+def generate_actual_data(data, year_start, year_finish):
+    countries = list(data["Country"].unique())
+    #countries.remove("Turkey")
+    #countries.remove("Norway")
+    columns = list(data.columns)
+    columns.remove("Year")
+    columns.remove("GDP")
+    columns.remove("Country")
+
+    result = pd.DataFrame()
+    for country in countries:
+        df = data[(data["Country"] == country) & (data["Year"] >=year_start)& (data["Year"] <=year_finish)]
+        for column in columns:
+            df[column] = df[column] * 0.01 * df["GDP"]
+        result = pd.concat([result, df])
+    return result
+
+def save_by_country(folder, data, tag):
+    countries = (data["Country"].unique())
+    for country in countries:
+        data[data["Country"] == country].to_excel(f"./{folder}/{country}({tag}).xlsx")
+
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import pandas as pd
 from functools import reduce
 from DataCreation import DataCreation
-from helper import descriptive_stats, generate_growth_data
+from helper import descriptive_stats, generate_growth_data, generate_actual_data, save_by_country
 base_fp = "./data/"
 exm = DataCreation(base_fp)
 exm.compose_df()
 tb= exm.final_db
 
-#descriptive_stats(data=exm, folder_desc="descriptive_ind", main_name="All_the_means")
-        
-generate_growth_data(tb)
+descriptive_stats(generate_actual_data(tb,1996, 2019), "descriptive_ind", "Means(actual)")
